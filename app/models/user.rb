90e68650626e10fee53b37c49e3bb3b2623744a3
Ben Coullie <bencoullie@gmail.com>
2015-12-14 06:47:15 +0900

    allowed blank bio and age user characteristics

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
 
   # Validations
   validates_length_of :username, :minimum => 3, :maximum => 20, :allow_blank => false
-  validates_length_of :bio, :maximum => 140
-  validates :age, :inclusion => { :in => 0..120, :message => "Let's be serious..." }
+  validates_length_of :bio, :maximum => 140, allow_blank: true
+  validates :age, :inclusion => { :in => 0..120, :message => "Let's be serious..." }, allow_blank: true
 
   has_many :posts, dependent: :destroy
 end
